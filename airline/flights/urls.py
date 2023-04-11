373516c1edc232732720e1638c09a6a24c5eb0c1
Jean5027 <jeanviana5027@gmail.com>
2023-04-11 16:31:55 -0300

    att

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from . import views
 
 
 urlpatterns = [
-    
+    path("", views.index, name="index.html"),
+    path("<int:flight_id>", views.flight, name="flight"),
+    path("<int:flight_id>/book", views.book, name="book"),
+
+
+
 
 ]
